d50671974b23269343decc421e4dfd3a7fa953a2
Rajat Satashiya <rajat09satashiya@gmail.com>
2025-05-04 22:10:04 +0530

    initial setup

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,8 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  server: {port: 8088},
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
